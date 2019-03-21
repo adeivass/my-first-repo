78bb3e30ec37522aa5813e1ec4c0cafa60646303
adeivass <workcould4me@gmail.com>
2019-03-21 00:58:59 -0400

    implemened ng2-google-charts successfully

--- google-charts/src/app/app.module.ts
+++ google-charts/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { Ng2GoogleChartsModule } from 'ng2-google-charts';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    Ng2GoogleChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
